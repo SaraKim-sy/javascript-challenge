c0bf57ea39b9e6818019ecade7c3f69905012bc5
Sooyeon  Kim <71349348+SaraKim-sy@users.noreply.github.com>
2021-01-14 22:25:57 -0500

    level2 progress

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,25 +1,40 @@
 // Assign the data from `data.js` to a descriptive variable
 var tableData = data;
 
-// List of cities
-let cities = tableData.map(ufoSighting => ufoSighting.city);
-console.log(cities);
-
 // Get a reference to the table body
 let tbody = d3.select("tbody");
 
+// The entire table to be displayed in the fist page
 tableData.forEach( (ufoSighting) => {
     // Use d3 to append one table row `tr` for each ufoSighting object
     let row = tbody.append("tr");
-
     // Use `Object.entries` and d3 to append 1 cell per each ufoSighting value
     Object.entries(ufoSighting).forEach( ([key, value]) => {
-        let cell = row.append("td");
-        
+        let cell = row.append("td");        
         // Use d3 to update each cell's text with ufoSighting values
         cell.text(value);
     });
 });
+
+// Populate dropdowns selections
+
+// List of cities, states, countries, shape (only unique values)
+let cities = Array.from(new Set(tableData.map(ufoSighting => ufoSighting.city)));
+let states = Array.from(new Set(tableData.map(ufoSighting => ufoSighting.state)));
+let countries = Array.from(new Set(tableData.map(ufoSighting => ufoSighting.country)));
+let shapes = Array.from(new Set(tableData.map(ufoSighting => ufoSighting.shape)));
+
+let selectState = d3.select("#select-state");
+let selectCountry = d3.select("#select-country");
+let selectShape = d3.select("#select-shape");
+
+states.forEach( state => selectState.append("option").text(state) );
+countries.forEach( country => selectCountry.append("option").text(country) );
+shapes.forEach( shape => selectShape.append("option").text(shape) );
+
+
+
+console.log(states);
 
 // Use a date form in your HTML document and write JavaScript code 
 // that will listen for events and search through the date/time column
